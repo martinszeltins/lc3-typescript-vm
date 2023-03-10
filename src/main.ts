f197e1e7c5e5ff79be16aca6e9ecbf0839a24ec2
Martins <martins.zeltins@arkbauer.com>
2023-03-10 22:33:36 +0200

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import {ProgramRunner} from './ProgramRunner';
-import fs = require('fs');
+import fs = require('fs')
+import { ProgramRunner } from './vm'
 
-let imageFileLocation = '/home/marshall/dev/github/lc3-2048/2048.obj';
-let imageData = fs.readFileSync(imageFileLocation);
-let imageDataUInt16 = new Uint16Array(imageData.length/2);
+let imageFileLocation = '/home/martins/Programming/lc3-typescript-vm/sample-lc3-program/2048.obj'
+let imageData = fs.readFileSync(imageFileLocation)
+let imageDataUInt16 = new Uint16Array(imageData.length / 2)
+
 for (var i = 0; i < imageData.length; i += 2) {
-    let uint16 = imageData[i] << 8 | imageData[i+1];
-    imageDataUInt16[i/2] = uint16;
+    let uint16 = imageData[i] << 8 | imageData[i+1]
+    imageDataUInt16[i / 2] = uint16
 }
 
-let runner = new ProgramRunner([]);
-runner.readImage(imageDataUInt16);
-runner.run();
+let runner = new ProgramRunner([])
+runner.readImage(imageDataUInt16)
+runner.run()
