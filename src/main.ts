15185a834005e06389619da27ee74a355d608633
Martins <martins.zeltins@arkbauer.com>
2023-03-11 00:25:45 +0200

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,22 @@ import fs = require('fs')
 import { VirtualMachine } from './vm'
 
 let programFileLocation = '/home/martins/Programming/lc3-typescript-vm/sample-lc3-program/2048.obj'
-let programData = fs.readFileSync(programFileLocation)
-let programDataUInt16 = new Uint16Array(programData.length / 2)
+let rawProgramData      = fs.readFileSync(programFileLocation)
+let programInstructions = new Uint16Array(rawProgramData.length / 2)
 
-for (var i = 0; i < programData.length; i += 2) {
-    let uint16 = (programData[i] * 256) + programData[i + 1];
-    programDataUInt16[i / 2] = uint16
+/**
+ * programInstructions = [
+ *    0011000000000000, // first instruction
+ *    0010110000010111, // second instruction
+ *    1110101000011000, // third instruction
+ *    ...
+ * ]
+ */
+for (var i = 0; i < rawProgramData.length; i += 2) {
+    let uint16 = (rawProgramData[i] * 256) + rawProgramData[i + 1];
+    programInstructions[i / 2] = uint16
 }
 
 let virtualMachine = new VirtualMachine([])
-virtualMachine.readProgram(programDataUInt16)
+virtualMachine.readProgram(programInstructions)
 virtualMachine.run()
